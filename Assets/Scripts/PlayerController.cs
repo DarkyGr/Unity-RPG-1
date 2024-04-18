f2c36532e04554658d8df5db991790378b8800f3
Guillermo Riaño <guilledark17@gmail.com>
2024-04-17 20:22:43 -0600

    📦 Configuration: Plyaer detect to NPC or Challenger message 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -58,7 +58,7 @@ public class PlayerController : MonoBehaviour
 
         var collider = Physics2D.OverlapCircle(interactPos, 0.2f, interactableLayer);
         if (collider != null)
-            Debug.Log("There is an NCP!");
+            collider.GetComponent<Interactable>()?.Interact();
     }
 
     IEnumerator Move(Vector3 targetPos)
