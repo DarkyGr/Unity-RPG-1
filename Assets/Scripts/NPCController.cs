1b5e9f8dec11868f636371844c76778be3f7796f
Guillermo Riaño <guilledark17@gmail.com>
2024-04-17 20:21:39 -0600

    ✅ Adding Interface: NPC and Challenger

---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour, Interactable
+{
+    public void Interact()
+    {
+        Debug.Log("Hello World, I'm an NPC!");
+    }
+}
